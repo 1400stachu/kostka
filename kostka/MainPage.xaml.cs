61315b38aabddc2ab5d230d498fcffc1944fd1fe
1900088 <1900088@TE-GDA-D-D21E23>
2024-09-16 10:18:42 +0200

    Dodaj pliki projektów.

---
+++ kostka/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace kostka
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            //wygenerowanie liczby losowej
+            Random r = new Random();
+            //genrujemy rzut kostką K6 - <1;6>
+            int kostka = r.Next(1, 7);
+            //wyświetlenie wyniku
+            RollValueLabel.Text = "Wysolowany rzut: " + kostka.ToString();
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ kostka/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace kostka
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
